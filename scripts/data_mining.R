49ef2221ea7d5de39b5e25aa2308ea52d28599fa
togoRyo3527 <ryoomori.usa2000@gmail.com>
2024-08-31 13:24:00 +0900

    good

--- scripts/data_mining.R
+++ scripts/data_mining.R
@@ -28,15 +28,29 @@ setwd("./rawdata/pitching/")
 csv_list <- list.files(pattern = "*.csv")
 data <- do.call(rbind, lapply(csv_list, function(x) read_csv(x)))
 data %>% 
-  mutate(IP = (IP - as.integer(IP))*10 + as.integer(IP)*3) %>% select(IP)
+  mutate(IP = (IP - as.integer(IP))*10 + as.integer(IP)*3) %>% 
   set_names(c(
     "year","team","playerID","name","age","win","lose","WL","ERA","game",
-    "starter","closer","complete","shutout","save","outs","hit","R","ER","HR",
-    "BB","IBB","K","HBP","BK","WP","BF","WHIP","H9","HR9","BB9","SO9","KBB" 
+    "starter","closer","complete","shutout","save","outs","hit","run","ER","HR",
+    "BB","IBB","K","HBP","BK","WP","BF","WHIP","H9","HR9","BB9","K9","KBB" 
   )) %>% 
   mutate_all(~replace(., is.na(.), -99999)) %>% 
   mutate_all(~replace(., is.infinite(.), -99999)) %>% 
-  relocate(single, .before = double) %>% 
   write_csv("../../data/PlayerPitchingByYear.csv")
 setwd("../..")
 
+# fielding あと回し ----------------------------------------------------------------
+setwd("./rawdata/fielding/")
+csv_list <- list.files(pattern = "*.csv")
+data <- do.call(rbind, lapply(csv_list, function(x) read_csv(x)))
+data %>% 
+  mutate(IP = (IP - as.integer(IP))*10 + as.integer(IP)*3) %>% 
+  set_names(c(
+    "year","team","playerID","name","age","win","lose","WL","ERA","game",
+    "starter","closer","complete","shutout","save","outs","hit","run","ER","HR",
+    "BB","IBB","K","HBP","BK","WP","BF","WHIP","H9","HR9","BB9","K9","KBB" 
+  )) %>% 
+  mutate_all(~replace(., is.na(.), -99999)) %>% 
+  mutate_all(~replace(., is.infinite(.), -99999)) %>% 
+  write_csv("../../data/PlayerFieldingByYear.csv")
+setwd("../..")
